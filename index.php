36036bbe03a78c0e7a71b82d85c98db146fb6edb
Prayoga Nanda Wiranata <masihkuliah@gmail.com>
2025-11-09 03:58:54 +0700

    Menambahkan file index.php di branch main

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head><title>Tambah Data</title></head>
+<body>
+<h1>Form Tambah Data</h1>
+<form>
+  <label>Nama:</label>
+  <input type='text' name='nama'><br>
+  <label>Email:</label>
+  <input type='email' name='email'><br>
+  <button type='submit'>Tambah</button>
+</form>
+</body>
+</html>
